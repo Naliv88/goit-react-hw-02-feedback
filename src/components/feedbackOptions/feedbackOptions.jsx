a15e801eaa69bcfe145d793dac1a72b4f3c5f791
Naliv88 <nalivpv@gmail.com>
2022-12-24 10:32:19 +0200

    style

--- src/components/feedbackOptions/feedbackOptions.jsx
+++ src/components/feedbackOptions/feedbackOptions.jsx
@@ -1,11 +1,13 @@
 import PropTypes from 'prop-types';
+import style from './FeedbackOptions.module.css';
 
 export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
   return (
-    <ul>
+    <ul className={style.list}>
       {options.map(option => (
         <li key={option}>
           <button
+            className={style.listBtn}
             type="button"
             name={option}
             onClick={onLeaveFeedback}
